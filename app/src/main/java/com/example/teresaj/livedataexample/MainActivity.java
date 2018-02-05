57026a120897bbea0dd7d87a5f43a79c237d618c
teresaj <teresa.jimenezarreola@thetrainline.com>
2018-02-05 10:51:25 +0000

    using viewmodel to update ui

--- app/src/main/java/com/example/teresaj/livedataexample/MainActivity.java
+++ app/src/main/java/com/example/teresaj/livedataexample/MainActivity.java
@@ -1,23 +1,52 @@
 package com.example.teresaj.livedataexample;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.nameTextView)
     TextView textView;
 
-    @BindView(R.id.button)
-    Button button;
+    @OnClick(R.id.button)
+    void submit() {
+        String value = "Jogn Doe " + counter;
+        counter++;
+        mModel.getCurrentName().setValue(value);
+    }
+
+    private NameViewModel mModel;
+
+    private int counter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mModel = ViewModelProviders.of(this).get(NameViewModel.class);
+
+        // Create the observer which updates the UI.
+        final Observer<String> nameObserver = new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable final String newName) {
+                // Update the UI, in this case, a TextView.
+                textView.setText(newName);
+            }
+        };
+
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        mModel.getCurrentName().observe(this, nameObserver);
+
     }
 }
--- app/src/main/java/com/example/teresaj/livedataexample/NameViewModel.java
+++ app/src/main/java/com/example/teresaj/livedataexample/NameViewModel.java
@@ -1,4 +1,17 @@
 package com.example.teresaj.livedataexample;
 
-public class NameViewModel {
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+public class NameViewModel extends ViewModel {
+
+    private MutableLiveData<String> mCurrentName;
+
+    public MutableLiveData<String> getCurrentName() {
+        if (mCurrentName == null) {
+            mCurrentName = new MutableLiveData<String>();
+        }
+        return mCurrentName;
+    }
+
 }
